16e0f8ce1f1695f84f2a5de97db18d7c0a3f4915
Juan Ruiz Cano <271003763+juanruizcano4@users.noreply.github.com>
2026-04-25 14:05:06 +0200

    pene

--- CodeProjects/Fraud2/src/ArrayClustering.cpp
+++ CodeProjects/Fraud2/src/ArrayClustering.cpp
@@ -12,3 +12,38 @@
  * Created on 22 de octubre de 2025, 11:57
  */
 
+/**
+ * @brief Deallocates the dynamic array of Clustering objects in the provided
+ * ArrayClustering. It also sets the clustering pointer to nullptr, and the
+ * capacity and size fields to 0.
+ * @param arrayClustering The ArrayClustering to deallocate. Output parameter
+ */
+void ArrayClustering::DeallocateArrayClustering(ArrayClustering arrayClustering){
+    delete[] arrayClustering.clustering; 
+    arrayClustering.clustering = nullptr; 
+    arrayClustering.capacity = 0; 
+    arrayClustering.size = 0; 
+
+}
+
+/**
+ * @brief Searches for a given Clustering object in the provided
+ * ArrayClustering. The search is performed by comparing each Clustering object
+ * in the array with the provided Clustering object using the
+ * Clustering::isEquivalentTo() method.
+ * @param arrayClustering The ArrayClustering where to search will be 
+ * performed. Input parameter
+ * @param clustering The Clustering object to search for. Input parameter
+ * @return The position of the Clustering object in the array if found;
+ * -1 otherwise.
+ */
+int ArrayClustering::FindArrayClustering(ArrayClustering arrayClustering, Clustering clustering) {
+    int pos = -1; 
+    for(int i = 0; i < arrayClustering.size; i++){ // Iterate through the array of Clustering objects
+        if(arrayClustering.clustering[i].isEquivalentTo(clustering)){ // If the current Clustering object is equivalent to the provided Clustering object
+            pos = i;  
+        }
+    }
+    return pos;  
+
+                                         
